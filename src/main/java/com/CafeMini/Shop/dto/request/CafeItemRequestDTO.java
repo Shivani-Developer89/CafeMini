de76449fd874a3eede3a57a6d5a0f3f80e0119f6
Shivani-Developer89 <your-email@example.com>
2025-07-30 18:06:42 +0530

    DTO for cafeItem for request and response

---
+++ src/main/java/com/CafeMini/Shop/dto/request/CafeItemRequestDTO.java
@@ -0,0 +1,11 @@
+package com.CafeMini.Shop.dto.request;
+
+public class CafeItemRequestDTO {
+    private String name;
+    private  String description;
+    private String category;
+    private  String size;
+    private  String SpicyLevel;
+    private double price;
+    private boolean available;
+}
---
+++ src/main/java/com/CafeMini/Shop/dto/response/CafeItemResponseDTO.java
@@ -0,0 +1,12 @@
+package com.CafeMini.Shop.dto.response;
+
+public class CafeItemResponseDTO {
+    private Long id;
+    private String name;
+    private  String description;
+    private String category;
+    private  String size;
+    private  String SpicyLevel;
+    private double price;
+    private boolean available;
+}
