de7602471d1d1f7ff9bdb54127ee51a91effba92
Shivani-Developer89 <shivani25782@gmail.com>
2025-07-31 17:39:15 +0530

    write into entity anotation

--- src/main/java/com/CafeMini/Shop/model/Reservation.java
+++ src/main/java/com/CafeMini/Shop/model/Reservation.java
@@ -1,11 +1,12 @@
 package com.CafeMini.Shop.model;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 import java.time.LocalDateTime;
-
+@Entity
 public class Reservation {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
