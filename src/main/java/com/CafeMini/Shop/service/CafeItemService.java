f1f41f511581396d09a4997ef241b0eb20a6da46
Shivani-Developer89 <shivani25782@gmail.com>
2025-08-05 00:00:01 +0530

    Cafe item service implementation

--- src/main/java/com/CafeMini/Shop/service/CafeItemService.java
+++ src/main/java/com/CafeMini/Shop/service/CafeItemService.java
@@ -2,14 +2,16 @@ package com.CafeMini.Shop.service;
 
 import com.CafeMini.Shop.dto.request.CafeItemRequestDTO;
 import com.CafeMini.Shop.dto.response.CafeItemResponseDTO;
+import com.CafeMini.Shop.model.CafeItem;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@Service
-public interface CafeItemService {
+@S
+public interface CafeItemService  {
+    CafeItemResponseDTO getCafeItem(Long id);
     CafeItemResponseDTO createCafeItem(CafeItemRequestDTO cafeItemRequestDTO);
-    CafeItemResponseDTO getCafeItem(CafeItemRequestDTO cafeItemRequestDTO);
     CafeItemResponseDTO updateCafeItem(CafeItemRequestDTO cafeItemRequestDTO);
     List<CafeItemResponseDTO> getAllCafeItem(CafeItemRequestDTO cafeItemRequestDTO);
     String removeCafeItem(Long id);
--- src/main/java/com/CafeMini/Shop/service/CafeItemServiceImpl.java
+++ src/main/java/com/CafeMini/Shop/service/CafeItemServiceImpl.java
@@ -1,4 +1,46 @@
 package com.CafeMini.Shop.service;
 
-public class CafeItemServiceImpl {
+import com.CafeMini.Shop.dto.request.CafeItemRequestDTO;
+import com.CafeMini.Shop.dto.response.CafeItemResponseDTO;
+import com.CafeMini.Shop.model.CafeItem;
+import com.CafeMini.Shop.repository.CafeItemRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.repository.query.FluentQuery;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Function;
+
+@Service
+public class CafeItemServiceImpl  implements  CafeItemService{
+    @Override
+    public CafeItemResponseDTO getCafeItem(Long id) {
+        return null;
+    }
+
+    @Override
+    public CafeItemResponseDTO createCafeItem(CafeItemRequestDTO cafeItemRequestDTO) {
+        return null;
+    }
+
+    @Override
+    public CafeItemResponseDTO updateCafeItem(CafeItemRequestDTO cafeItemRequestDTO) {
+        return null;
+    }
+
+    @Override
+    public List<CafeItemResponseDTO> getAllCafeItem(CafeItemRequestDTO cafeItemRequestDTO) {
+        return List.of();
+    }
+
+    @Override
+    public String removeCafeItem(Long id) {
+        return "";
+    }
 }
+
