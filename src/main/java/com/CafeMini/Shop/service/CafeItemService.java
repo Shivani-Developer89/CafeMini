be24be6a51d472383a01b7405d8c1bc06a51eb64
Shivani-Developer89 <shivani25782@gmail.com>
2025-08-05 02:38:43 +0530

    Make changes for acceptance

--- src/main/java/com/CafeMini/Shop/service/CafeItemService.java
+++ src/main/java/com/CafeMini/Shop/service/CafeItemService.java
@@ -2,16 +2,14 @@ package com.CafeMini.Shop.service;
 
 import com.CafeMini.Shop.dto.request.CafeItemRequestDTO;
 import com.CafeMini.Shop.dto.response.CafeItemResponseDTO;
-import com.CafeMini.Shop.model.CafeItem;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@S
+@Service
 public interface CafeItemService  {
     CafeItemResponseDTO getCafeItem(Long id);
-    CafeItemResponseDTO createCafeItem(CafeItemRequestDTO cafeItemRequestDTO);
+    CafeItemResponseDTO addCafeItem(CafeItemRequestDTO cafeItemRequestDTO);
     CafeItemResponseDTO updateCafeItem(CafeItemRequestDTO cafeItemRequestDTO);
     List<CafeItemResponseDTO> getAllCafeItem(CafeItemRequestDTO cafeItemRequestDTO);
     String removeCafeItem(Long id);
