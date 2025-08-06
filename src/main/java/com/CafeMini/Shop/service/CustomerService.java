b6e56727fcc5c14fc1644fdc0a9502a35c586475
Shivani-Developer89 <shivani25782@gmail.com>
2025-08-06 23:55:52 +0530

    Create method and logic

--- src/main/java/com/CafeMini/Shop/service/CustomerService.java
+++ src/main/java/com/CafeMini/Shop/service/CustomerService.java
@@ -1,4 +1,19 @@
 package com.CafeMini.Shop.service;
 
+import com.CafeMini.Shop.dto.request.CustomerRequestDTO;
+import com.CafeMini.Shop.dto.response.CustomerResponseDTO;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
 public interface CustomerService {
+    CustomerResponseDTO createCustomer(CustomerRequestDTO customerRequestDTO);
+
+    CustomerResponseDTO getCustomer(Long id);
+
+    List<CustomerResponseDTO> getAllCustomers();
+
+    CustomerResponseDTO updateCustomer(Long id, CustomerRequestDTO customerRequestDTO);
+
+    void deleteCustomer(Long id);
 }
--- src/main/java/com/CafeMini/Shop/service/CustomerServiceImpl.java
+++ src/main/java/com/CafeMini/Shop/service/CustomerServiceImpl.java
@@ -1,4 +1,40 @@
 package com.CafeMini.Shop.service;
 
-public class CustomerServiceImpl {
+import com.CafeMini.Shop.dto.request.CustomerRequestDTO;
+import com.CafeMini.Shop.dto.response.CustomerResponseDTO;
+import com.CafeMini.Shop.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CustomerServiceImpl  implements CustomerService{
+   @Autowired
+    CustomerRepository customerRepository;
+
+    @Override
+    public CustomerResponseDTO createCustomer(CustomerRequestDTO customerRequestDTO) {
+        return null;
+    }
+
+    @Override
+    public CustomerResponseDTO getCustomer(Long id) {
+        return null;
+    }
+
+    @Override
+    public List<CustomerResponseDTO> getAllCustomers() {
+        return List.of();
+    }
+
+    @Override
+    public CustomerResponseDTO updateCustomer(Long id, CustomerRequestDTO customerRequestDTO) {
+        return null;
+    }
+
+    @Override
+    public void deleteCustomer(Long id) {
+
+    }
 }
