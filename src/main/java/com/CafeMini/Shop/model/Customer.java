f676017bc02602c8166978ac42822593c3fd4c5b
Shivani-Developer89 <shivani25782@gmail.com>
2025-08-06 23:54:48 +0530

    Setting Id genrated anonation

--- src/main/java/com/CafeMini/Shop/model/Customer.java
+++ src/main/java/com/CafeMini/Shop/model/Customer.java
@@ -1,10 +1,16 @@
 package com.CafeMini.Shop.model;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import lombok.Data;
 
 @Entity
+@Data
+@Table(name = "Customer")
 public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private  Long id;
+
     private  String name;
     private String email;
     private String phone;
