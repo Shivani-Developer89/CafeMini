c4dbab9d8450d8f104a17af08b791802ec41118b
Shivani-Developer89 <shivani25782@gmail.com>
2025-08-07 21:07:31 +0530

    Completed the methods

--- src/main/java/com/CafeMini/Shop/service/CafeItemServiceImpl.java
+++ src/main/java/com/CafeMini/Shop/service/CafeItemServiceImpl.java
@@ -7,6 +7,7 @@ import com.CafeMini.Shop.repository.CafeItemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -50,23 +51,74 @@ public class CafeItemServiceImpl  implements  CafeItemService{
           CafeItemResponseDTO cafeItemResponseDTO = new CafeItemResponseDTO();
 
           cafeItemResponseDTO.setId(cafeItem.getId());
+          cafeItemResponseDTO.setName(cafeItem.getName());
+          cafeItemResponseDTO.setDescription(cafeItem.getDescription());
+          cafeItemResponseDTO.setCategory(cafeItem.getCategory());
+          cafeItemResponseDTO.setSize(cafeItem.getSize());
+          cafeItemResponseDTO.setSpicyLevel(cafeItem.getSpicyLevel());
+          cafeItemResponseDTO.setPrice(cafeItem.getPrice());
+          cafeItemResponseDTO.setAvailable(cafeItem.isAvailable());
           return  cafeItemResponseDTO;
 
     }
 
     @Override
-    public CafeItemResponseDTO updateCafeItem(CafeItemRequestDTO cafeItemRequestDTO) {
-        return null;
+    public CafeItemResponseDTO updateCafeItem(Long id ,CafeItemRequestDTO cafeItemRequestDTO) {
+        CafeItem cafeItem = new CafeItem();
+
+
+        cafeItem.setName(cafeItemRequestDTO.getName());
+        cafeItem.setDescription(cafeItemRequestDTO.getDescription());
+        cafeItem.setCategory(cafeItemRequestDTO.getCategory());
+        cafeItem.setSize(cafeItemRequestDTO.getSize());
+        cafeItem.setSpicyLevel(cafeItemRequestDTO.getSpicyLevel());
+        cafeItem.setPrice(cafeItem.getPrice());
+        cafeItem.setAvailable(cafeItem.isAvailable());
+
+        cafeItem = cafeItemRepository.save(cafeItem);
+
+        CafeItemResponseDTO cafeItemResponseDTO = new CafeItemResponseDTO();
+
+        cafeItemResponseDTO.setId(cafeItem.getId());
+        cafeItemResponseDTO.setName(cafeItem.getName());
+        cafeItemResponseDTO.setDescription(cafeItem.getDescription());
+        cafeItemResponseDTO.setCategory(cafeItem.getCategory());
+        cafeItemResponseDTO.setSize(cafeItem.getSize());
+        cafeItemResponseDTO.setSpicyLevel(cafeItem.getSpicyLevel());
+        cafeItemResponseDTO.setPrice(cafeItem.getPrice());
+        cafeItemResponseDTO.setAvailable(cafeItem.isAvailable());
+        return  cafeItemResponseDTO;
     }
 
     @Override
     public List<CafeItemResponseDTO> getAllCafeItem(CafeItemRequestDTO cafeItemRequestDTO) {
-        return List.of();
+        List<CafeItem> cafeItemList = cafeItemRepository.findAll();
+        List<CafeItemResponseDTO> cafeItemResponseDTOList = new ArrayList<>();
+         for(CafeItem cafeItem :cafeItemList){
+             CafeItemResponseDTO cafeItemResponseDTO= new CafeItemResponseDTO();
+
+             cafeItemResponseDTO.setId(cafeItem.getId());
+             cafeItemResponseDTO.setName(cafeItem.getName());
+             cafeItemResponseDTO.setDescription(cafeItem.getDescription());
+             cafeItemResponseDTO.setCategory(cafeItem.getCategory());
+             cafeItemResponseDTO.setSize(cafeItem.getSize());
+             cafeItemResponseDTO.setSpicyLevel(cafeItem.getSpicyLevel());
+             cafeItemResponseDTO.setPrice(cafeItem.getPrice());
+             cafeItemResponseDTO.setAvailable(cafeItem.isAvailable());
+
+             cafeItemResponseDTOList.add(cafeItemResponseDTO);
+         }
+
+        return cafeItemResponseDTOList;
     }
 
     @Override
     public String removeCafeItem(Long id) {
-        return "";
+        String name = cafeItemRepository.findById(id).orElse(null).getName();
+        cafeItemRepository.deleteById(id);
+        return  " CafeItem name : " + name + " and ID: " +id + "has been removed successfully!";
+
+
     }
 }
 
