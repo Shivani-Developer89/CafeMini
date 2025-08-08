d14869566f1d2db997c044d5c75e9592b4d01a74
Shivani-Developer89 <shivani25782@gmail.com>
2025-08-08 21:18:24 +0530

    Create Method

--- src/main/java/com/CafeMini/Shop/service/OrderItemService.java
+++ src/main/java/com/CafeMini/Shop/service/OrderItemService.java
@@ -1,4 +1,18 @@
 package com.CafeMini.Shop.service;
 
+import com.CafeMini.Shop.dto.request.OrderItemRequestDTO;
+import com.CafeMini.Shop.dto.response.OrderItemResponseDTO;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public interface OrderItemService {
+    OrderItemResponseDTO addOrder(OrderItemRequestDTO orderItemRequestDTO);
+    OrderItemResponseDTO getOrder(Long id);
+   List <OrderItemResponseDTO>getAllOrder();
+   OrderItemResponseDTO updateOrder(Long id ,OrderItemRequestDTO orderItemRequestDTO);
+   String removeOrder(Long id);
+
+
 }
