68b8ec090e5f43e01d8c5040c85bfda580095bf3
Shivani-Developer89 <shivani25782@gmail.com>
2025-08-08 21:19:16 +0530

    Create add orderItem logic

--- src/main/java/com/CafeMini/Shop/service/OrderItemServiceImpl.java
+++ src/main/java/com/CafeMini/Shop/service/OrderItemServiceImpl.java
@@ -1,4 +1,51 @@
 package com.CafeMini.Shop.service;
 
-public class OrderItemServiceImpl {
+import com.CafeMini.Shop.dto.request.OrderItemRequestDTO;
+import com.CafeMini.Shop.dto.response.OrderItemResponseDTO;
+import com.CafeMini.Shop.model.OrderItem;
+import com.CafeMini.Shop.repository.OrderItemRepository;
+import org.hibernate.query.Order;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+public class OrderItemServiceImpl  implements OrderItemService{
+    @Autowired
+    OrderItemRepository orderItemRepository;
+    @Override
+    public OrderItemResponseDTO addOrder(OrderItemRequestDTO orderItemRequestDTO) {
+        OrderItem orderItem = new OrderItem();
+
+        orderItem.setQuantity(orderItemRequestDTO.getQuantity());
+        orderItem.setItemPrice(orderItemRequestDTO.getItemPrice());
+
+        orderItem =orderItemRepository.save(orderItem);
+
+        OrderItemResponseDTO orderItemResponseDTO = new OrderItemResponseDTO();
+
+        orderItemResponseDTO.setQuantity(orderItem.getQuantity());
+        orderItemResponseDTO.setItemPrice(orderItem.getItemPrice());
+
+        return orderItemResponseDTO;
+    }
+
+    @Override
+    public OrderItemResponseDTO getOrder(Long id) {
+        return null;
+    }
+
+    @Override
+    public List<OrderItemResponseDTO> getAllOrder() {
+        return List.of();
+    }
+
+    @Override
+    public OrderItemResponseDTO updateOrder(Long id, OrderItemRequestDTO orderItemRequestDTO) {
+        return null;
+    }
+
+    @Override
+    public String removeOrder(Long id) {
+        return "";
+    }
 }
