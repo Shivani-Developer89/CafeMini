a512dfd674d3ad9df9751e2616b56d0f4407e6f1
Shivani-Developer89 <shivani25782@gmail.com>
2025-08-10 23:42:33 +0530

    Item name enum helps to order

---
+++ src/main/java/com/CafeMini/Shop/model/enums/ItemName.java
@@ -0,0 +1,35 @@
+package com.CafeMini.Shop.model.enums;
+
+public enum ItemName {
+
+        // ☕ Coffee & Beverages
+        ESPRESSO,
+        CAPPUCCINO,
+        LATTE_MACCHIATO,
+        MOCHA_DELIGHT,
+        ICED_AMERICANO,
+        MATCHA_LATTE,
+
+        // 🥐 Pastries & Snacks
+        CROISSANT,
+        CHOCOLATE_MUFFIN,
+        ALMOND_DANISH,
+        BLUEBERRY_SCONE,
+        BAGEL_WITH_CREAM_CHEESE,
+        CINNAMON_ROLL,
+
+        // 🍰 Desserts
+        NEW_YORK_CHEESECAKE,
+        TIRAMISU,
+        CHOCOLATE_LAVA_CAKE,
+        STRAWBERRY_TART,
+        BROWNIE_SUNDAE,
+
+        // 🥤 Cold & Refreshing
+        MANGO_SMOOTHIE,
+        BERRY_BLAST,
+        LEMON_ICED_TEA,
+        COLD_BREW
+    }
+
+
