5f6bb3203f71086c199175fe22817b0d33e0c2e2
Shivani-Developer89 <shivani25782@gmail.com>
2025-08-11 23:58:05 +0530

    make logic to create reservation

--- src/main/java/com/CafeMini/Shop/service/ReservationService.java
+++ src/main/java/com/CafeMini/Shop/service/ReservationService.java
@@ -1,5 +1,16 @@
 package com.CafeMini.Shop.service;
 
+import com.CafeMini.Shop.dto.request.ReservationRequestDTO;
+import com.CafeMini.Shop.dto.response.OrderItemResponseDTO;
+import com.CafeMini.Shop.dto.response.ReservationResponseDTO;
+
+import java.util.List;
+
 public interface ReservationService {
+    ReservationResponseDTO createReservation(ReservationRequestDTO reservationRequestDTO);
+    ReservationResponseDTO getReservation(Long id);
+    List<ReservationResponseDTO> getAllReservation();
+    ReservationResponseDTO updateReservation(Long id ,ReservationRequestDTO reservationRequestDTO);
+    String removeReservation(Long id);
 
 }
