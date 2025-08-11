ac0501466808391eac4fb788a06da1c03cc1c923
Shivani-Developer89 <shivani25782@gmail.com>
2025-08-11 23:54:49 +0530

    write table annotation

--- src/main/java/com/CafeMini/Shop/model/OrderItem.java
+++ src/main/java/com/CafeMini/Shop/model/OrderItem.java
@@ -1,13 +1,11 @@
 package com.CafeMini.Shop.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Data;
 
 @Entity
 @Data
+@Table(name = "OrderItem")
 public class OrderItem {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
