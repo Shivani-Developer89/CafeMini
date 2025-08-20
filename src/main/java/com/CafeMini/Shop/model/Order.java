17ea944788a4dcda4f13b6aaa8c9397140a422af
Shivani-Developer89 <shivani25782@gmail.com>
2025-08-20 23:59:54 +0530

    Create a Order class to built a relationship with cafeItem and OrderItem

---
+++ src/main/java/com/CafeMini/Shop/model/Order.java
@@ -0,0 +1,25 @@
+package com.CafeMini.Shop.model;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "Orders") // avoid conflict with SQL keyword "Order"
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "customer_id")
+    private Customer customer;
+
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
+    private List<OrderItem> orderItems = new ArrayList<>();
+
+    private LocalDateTime orderDate;
+    private double totalPrice;
+}
