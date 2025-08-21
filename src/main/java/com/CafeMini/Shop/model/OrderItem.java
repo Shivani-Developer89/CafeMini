193349e4ccb1662bbfd4ae39e7ddda67883d4b21
Shivani-Developer89 <shivani25782@gmail.com>
2025-08-21 19:56:58 +0530

    Write Data annotation  and built relationship

--- src/main/java/com/CafeMini/Shop/model/OrderItem.java
+++ src/main/java/com/CafeMini/Shop/model/OrderItem.java
@@ -4,19 +4,28 @@ import com.CafeMini.Shop.model.enums.ItemName;
 import jakarta.persistence.*;
 import lombok.Data;
 
-
-
-@Entity
 @Data
+@Entity
 @Table(name = "OrderItem")
 public class OrderItem {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Enumerated(EnumType.STRING)
-    private ItemName orderItemName;
+
     private int quantity;
     private double itemPrice;
 
+    // link to CafeItem
+    @ManyToOne
+    @JoinColumn(name = "cafe_item_id")
+    private CafeItem cafeItem;
 
+    // link to Order
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
 }
+
+
+
+
